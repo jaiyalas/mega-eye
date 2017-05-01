a741d6d979c977c17e6110ecf3a146d4e2990df2
Yun-Yan Chi <jaiyalas@gmail.com>
2017-05-01 17:58:37 +0800

    add sfml demo (simply window display); add remote SFML config in stack

--- dofu/app/Main.hs
+++ dofu/app/Main.hs
@@ -1,6 +1,28 @@
 module Main where
 
 import Lib
+import SFML.Window
 
 main :: IO ()
-main = someFunc
+main = do
+    desktopMode <- getDesktopMode
+    fsModes <- getFullscreenModes
+
+    putStrLn $ "Current desktop mode:\n\n" ++ show desktopMode
+    putStrLn ""
+    putStrLn $ "Fullscreen modes:"
+    putStrLn ""
+    mapM_ (\m -> putStrLn (show m) >> putStrLn "") fsModes
+
+    let ctxSettings = Just $ ContextSettings 24 8 0 1 2 [ContextDefault]
+    wnd <- createWindow (VideoMode 640 480 32) "SFML Haskell Demo" [SFDefaultStyle] ctxSettings
+    loop wnd
+    destroy wnd
+
+
+loop :: Window -> IO ()
+loop wnd = do
+    evt <- waitEvent wnd
+    case evt of
+        Just SFEvtClosed -> return ()
+        _ -> loop wnd
