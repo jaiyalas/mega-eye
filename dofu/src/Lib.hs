3cd2dec733b8931c4667ce9ab377712da43df00e
Yun-Yan Chi <jaiyalas@gmail.com>
2017-05-10 19:53:20 +0800

    editing

--- dofu/src/Lib.hs
+++ dofu/src/Lib.hs
@@ -1,6 +1,33 @@
-module Lib
-    ( someFunc
-    ) where
+{-# LANGUAGE TypeFamilies #-}
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+module Lib
+
+import SFML.Graphics as SF
+import Spine.C as SpineC
+
+
+-- SF.RenderTarget
+-- SF.RenderStates
+
+data SkeletonDrawableData =
+    { skeleton :: SpineC.Skeleton
+    , state :: SpineC.AnimationState
+    , timeScale :: Float
+    , vertexArray :: SF.VertexArray
+	, ownsAnimationStateData :: Boolean
+	, worldVertices :: Float
+    } deriving (Show, Eq)
+--
+newSkeletonDrawableData :: MonadIO m => SpineC.SkeletonData -> SpineC.AnimationStateData -> m a
+newSkeletonDrawableData = undefined
+--
+delSkeletonDrawableData :: MonadIO m => a -> m ()
+delSkeletonDrawableData = undefined
+--
+
+class SkeletonDrawable a b where
+    data RenderTarget :: * -> *
+    update :: MonadIO m => a -> Float -> m a
+    draw   :: MonadIO m => a -> renderTarget -> renderStates -> m a
+
+instance SkeletonDrawable
